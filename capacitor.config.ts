48f2538c97aca40b94f4f50cfaf657b9d0feaf8d
Slipromise <chengchun05@gmail.com>
2023-02-21 13:43:15 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'wallet.dapp',
+  appName: 'wallet-dapp',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
